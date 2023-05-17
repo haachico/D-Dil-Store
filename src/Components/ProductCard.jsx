7e1c3a8e8835114a9cba3a821d80addcd4f10807
Nilesh <nilesh@haachico.com>
2023-05-17 08:24:27 +0530

    Added icons to product card

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,6 +1,41 @@
+import { useState, useContext } from "react";
+
+import { Context } from "..";
+
 const ProductCard = ({ img, id, title, rating, price }) => {
+  const {
+    isHearted,
+    isAddedToCCart,
+    handleHeartIconClick,
+    handleHeartedIconClick,
+    handleAddToCartIconClick,
+    handleAddedToCartIconClick,
+  } = useContext(Context);
+
+  const heartReact = (
+    <i class="fa-regular fa-heart" onClick={handleHeartIconClick}></i>
+  );
+  const reactedHeart = (
+    <i class="fa-solid fa-heart" onClick={handleHeartedIconClick}></i>
+  );
+
+  const addToCart = (
+    <i class="fa-solid fa-cart-plus" onClick={handleAddToCartIconClick}></i>
+  );
+
+  const addedToCart = (
+    <i
+      class="fa-solid fa-cart-shopping"
+      onClick={handleAddedToCartIconClick}
+    ></i>
+  );
+
   return (
     <div className="product--card">
+      <div className="product--icons">
+        <p>{isHearted ? reactedHeart : heartReact}</p>
+        <p>{isAddedToCCart ? addedToCart : addToCart}</p>
+      </div>
       <img src={img} alt={title} />
       <h4>{title}</h4>
       <hr className="break--line" />
--- src/useContext/Context.jsx
+++ src/useContext/Context.jsx
@@ -5,6 +5,24 @@ export const Context = createContext();
 
 export const Provider = ({ children }) => {
   const [data, setData] = useState([]);
+  const [isHearted, setIsHearted] = useState(false);
+  const [isAddedToCCart, setIsAddedToCart] = useState(false);
+
+  const handleHeartIconClick = () => {
+    setIsHearted(true);
+  };
+
+  const handleHeartedIconClick = () => {
+    setIsHearted(false);
+  };
+
+  const handleAddToCartIconClick = () => {
+    setIsAddedToCart(true);
+  };
+
+  const handleAddedToCartIconClick = () => {
+    setIsAddedToCart(false);
+  };
   const getData = async () => {
     try {
       const response = await allData;
@@ -21,7 +39,19 @@ export const Provider = ({ children }) => {
 
   return (
     <div>
-      <Context.Provider value={{ data }}>{children}</Context.Provider>
+      <Context.Provider
+        value={{
+          data,
+          isHearted,
+          isAddedToCCart,
+          handleHeartIconClick,
+          handleHeartedIconClick,
+          handleAddToCartIconClick,
+          handleAddedToCartIconClick,
+        }}
+      >
+        {children}
+      </Context.Provider>
     </div>
   );
 };
