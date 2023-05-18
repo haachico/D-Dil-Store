4650361c5f24f018830e275f7c2465ac873f3647
Nilesh <nilesh@haachico.com>
2023-05-18 13:27:06 +0530

    added count to icons in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+
+import { Context } from "..";
 import logoImg from "./logo.png";
 
 const Header = () => {
+  const { wishlistItems, cartItems } = useContext(Context);
   return (
     <div className="header">
       <NavLink to="/">
@@ -16,10 +20,16 @@ const Header = () => {
           <i class="fa-solid fa-info"></i>
         </NavLink>
         <NavLink to="wishlist">
-          <i class="fa-solid fa-heart"></i>
+          <i class="fa-solid fa-heart wishlistIcon"></i>
+          <span className="wishlistBadge">
+            {wishlistItems.length > 0 && wishlistItems.length}
+          </span>
         </NavLink>
         <NavLink to="cart">
-          <i class="fa-solid fa-cart-shopping"></i>
+          <i class="fa-solid fa-cart-shopping cartIcon"></i>
+          <span className="cartBadge">
+            {cartItems.length > 0 && cartItems.length}
+          </span>
         </NavLink>
         <NavLink to="auth">
           <i class="fa-solid fa-user"></i>
