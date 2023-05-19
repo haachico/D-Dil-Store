3ad2b4f4a36f0f397ef450edd95eb66fc26e9208
Nilesh <nilesh@haachico.com>
2023-05-19 19:17:56 +0530

    Added to Reviews & About us pages

--- src/Pages/Reviews.jsx
+++ src/Pages/Reviews.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
+import { ReviewsData } from "../ReviewsData";
+
 const Reviews = () => {
+  const [reviewsData, setReviewsData] = useState([]);
+
+  useEffect(() => {
+    const results = ReviewsData;
+    setReviewsData(results);
+  }, []);
+
   return (
-    <div>
-      <h1>Reviews will come here</h1>
+    <div className="review--page">
+      <h2>Some Reviews from our esteemed customers!</h2>
+      {reviewsData.map((e) => (
+        <div className="review--card">
+          <q>{e.review}</q>
+          <p style={{ fontSize: "12px" }}>
+            <strong>---{e.user}</strong>
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
