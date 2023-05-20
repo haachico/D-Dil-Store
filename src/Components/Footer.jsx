bb627dbbab05ad33dedc4be55de4a0e800898b01
Nilesh <nilesh@haachico.com>
2023-05-20 10:27:01 +0530

    Added a footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+  return (
+    <div className="footer--page">
+      <p>Created with 🤍 by haachico</p>
+      <div className="socialmedia--icons">
+        <p>
+          <a href="https://www.linkedin.com/in/nilesh-kokare-92461b231/">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+        </p>
+        <p>
+          <a href="https://twitter.com/haachico">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+        </p>
+        <p>
+          <a href="https://www.instagram.com/haachico/">
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
